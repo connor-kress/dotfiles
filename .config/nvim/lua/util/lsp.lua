432600535402a74a20a6d6ec62bfd42b97c216f0
Connor Kress <con2coool@gmail.com>
2024-07-01 16:37:50 -0400

    Fixed bug with keymaps.

--- .config/nvim/lua/util/lsp.lua
+++ .config/nvim/lua/util/lsp.lua
@@ -1,23 +1,21 @@
-local keymap = vim.keymap
-
 local M = {}
 
 M.on_attach = function(client, buffer)
   local opts = { noremap = true, silent = true, buffer = buffer }
 
-  keymap("<leader>fd", "Lspsaga finder", "n", opts) -- go to definition
-  keymap("<leader>gd", "Lspsaga peek_definition", "n", opts) -- peak definition
-  keymap("<leader>gD", "Lspsaga goto_definition", "n", opts) -- go to definition
-  keymap("<leader>ca", "Lspsaga code_action", "n", opts) -- see available code actions
-  keymap("<leader>rn", "Lspsaga rename", "n", opts) -- smart rename
-  keymap("<leader>D", "Lspsaga show_line_diagnostics", "n", opts) -- show  diagnostics for line
-  keymap("<leader>d", "Lspsaga show_cursor_diagnostics", "n", opts) -- show diagnostics for cursor
-  keymap("<leader>pd", "Lspsaga diagnostic_jump_prev", "n", opts) -- jump to prev diagnostic in buffer
-  keymap("<leader>nd", "Lspsaga diagnostic_jump_next", "n", opts) -- jump to next diagnostic in buffer
-  keymap("K", "Lspsaga hover_doc", "n", opts) -- show documentation for what is under cursor
+  vim.keymap.set("n", "<leader>fd", "Lspsaga finder", opts) -- go to definition
+  vim.keymap.set("n", "<leader>gd", "Lspsaga peek_definition", opts) -- peak definition
+  vim.keymap.set("n", "<leader>gD", "Lspsaga goto_definition", opts) -- go to definition
+  vim.keymap.set("n", "<leader>ca", "Lspsaga code_action", opts) -- see available code actions
+  vim.keymap.set("n", "<leader>rn", "Lspsaga rename", opts) -- smart rename
+  vim.keymap.set("n", "<leader>D", "Lspsaga show_line_diagnostics", opts) -- show  diagnostics for line
+  vim.keymap.set("n", "<leader>d", "Lspsaga show_cursor_diagnostics", opts) -- show diagnostics for cursor
+  vim.keymap.set("n", "<leader>pd", "Lspsaga diagnostic_jump_prev", opts) -- jump to prev diagnostic in buffer
+  vim.keymap.set("n", "<leader>nd", "Lspsaga diagnostic_jump_next", opts) -- jump to next diagnostic in buffer
+  vim.keymap.set("n", "K", "Lspsaga hover_doc", opts) -- show documentation for what is under cursor
 
   if client.name == "pyright" then
-      keymap("<Leader>oi", "PyrightOrganizeImports", "n", opts)
+      vim.keymap.set("n", "<Leader>oi", "PyrightOrganizeImports", opts)
   end
 end
 
