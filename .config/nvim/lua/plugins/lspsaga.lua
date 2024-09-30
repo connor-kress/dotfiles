c6a70e489dd611d5d5429cd711aba41c216d3ec4
Connor Kress <con2coool@gmail.com>
2024-09-29 22:23:28 -0400

    Indentation fix

--- .config/nvim/lua/plugins/lspsaga.lua
+++ .config/nvim/lua/plugins/lspsaga.lua
@@ -1,25 +1,25 @@
 return {
-	"glepnir/lspsaga.nvim",
-	lazy = false,
-	config = function()
-		require("lspsaga").setup({
-            -- remove lightbulb symbols
-            ui = {
-                code_action = "",
-            },
-			-- keybinds for navigation in lspsaga window
-			move_in_saga = { prev = "<C-k>", next = "<C-j>" },
-			-- use enter to open file with finder
-			finder_action_keys = {
-				open = "<CR>",
-			},
-			-- use enter to open file with definition preview
-			definition_action_keys = {
-				edit = "<CR>",
-			},
-		})
-	end,
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter",
-	},
+    "glepnir/lspsaga.nvim",
+    lazy = false,
+    config = function()
+    require("lspsaga").setup({
+        -- remove lightbulb symbols
+        ui = {
+            code_action = "",
+        },
+        -- keybinds for navigation in lspsaga window
+        move_in_saga = { prev = "<c-k>", next = "<c-j>" },
+        -- use enter to open file with finder
+        finder_action_keys = {
+            open = "<cr>",
+        },
+        -- use enter to open file with definition preview
+        definition_action_keys = {
+            edit = "<cr>",
+        },
+    })
+    end,
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+    },
 }
