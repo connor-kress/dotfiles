3f41099e534c927f4e5d05ac9af109014c456fc4
Connor Kress <ckress@ufl.edu>
2025-12-21 23:14:41 -0500

    `mpv`: load bass and treble from "af" property

--- .config/mpv/scripts/bass_treble.lua
+++ .config/mpv/scripts/bass_treble.lua
@@ -1,8 +1,44 @@
 local mp = require("mp")
+local msg = require("mp.msg")
 
+local function parse_number_after(s, key)
+    local i = s:find(key, 1, true)
+    if not i then
+        return nil
+    end
+    i = i + #key
+    local j = i
+
+    while j <= #s do
+        local c = s:sub(j, j)
+        if not (c:match("[%d%.%-]")) then
+            break
+        end
+        j = j + 1
+    end
+    return tonumber(s:sub(i, j - 1))
+end
+
+local function parse_bass_treble_from_af()
+    local af = mp.get_property("af")
+    msg.debug("Raw af: " .. tostring(af))
+    if not af or af == "" then
+        return 0, 0
+    end
+    local bass = parse_number_after(af, "bass=g=")
+    local treble = parse_number_after(af, "treble=g=")
+    return bass or 0, treble or 0
+end
+
+local step = 2
 local bass = 0
 local treble = 0
-local step = 2
+
+mp.register_event("file-loaded", function()
+    -- properties restored, tracks selected, watch-later applied
+    bass, treble = parse_bass_treble_from_af()
+    -- msg.info("Bass: " .. bass .. " dB, Treble: " .. treble .. " dB")
+end)
 
 local function display_bass()
     mp.osd_message(string.format("Bass: %d dB", bass), 1.5)
